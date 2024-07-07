ec47200d49139e268f81fb66515e378491e0c360
Will <will-blk@hotmail.com>
2024-07-07 09:53:10 -0300

    Add presence validation to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  validates :name,
+            :cpf,
+            :phone,
+            :address,
+            :zipcode, presence: true
   validates_with CpfValidator
 end
